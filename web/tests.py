2b941565456f6f806054e120746c6bea1183ead5
Pietro Di Bello <pietro.dibello@xpeppers.com>
2015-02-11 23:56:15 +0100

    Trying to broke the build...

--- web/tests.py
+++ web/tests.py
@@ -5,8 +5,7 @@ class MainTestCase(unittest.TestCase):
 
     def test_two_and_two(self):
         four = 2 + 2
-        self.assertEqual(four, 4)
-        self.assertNotEqual(four, 5)
+        self.assertEqual(four, 5)
 
 
 if __name__ == '__main__':
